494fe18813a8f75b064bc46346973d180506ee55
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-03-29 12:42:03 -0300

    add dummy channel component & stories

---
+++ packages/site/src/components/Channel.tsx
@@ -0,0 +1,10 @@
+interface ChannelProps {
+  funded: boolean;
+}
+
+export const Channel = ({ funded = false }: ChannelProps) => {
+  if (funded) {
+    return <div>Looks good</div>;
+  }
+  return <div>No funds</div>;
+};
---
+++ packages/site/src/stories/Channel.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Channel } from '../components/Channel';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Channel',
+  component: Channel,
+} as ComponentMeta<typeof Channel>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Channel> = (args: object) => (
+  <Channel {...args} />
+);
+
+export const Funded = Template.bind({});
+Funded.args = {
+  funded: true,
+};
+
+export const NotFunded = Template.bind({});
+NotFunded.args = {
+  funded: false,
+};
