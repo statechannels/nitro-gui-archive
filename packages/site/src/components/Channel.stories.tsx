7268faa31fd57fc3304a6790786b98853b6c59e2
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-03-29 12:57:29 -0300

    move channel story to correct location

--- packages/site/src/components/Channel.stories.tsx
+++ packages/site/src/components/Channel.stories.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Channel, ChannelProps } from '../components/Channel';
+import { Channel, ChannelProps } from './Channel';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Channel',
   component: Channel,
 } as ComponentMeta<typeof Channel>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Channel> = (args: ChannelProps) => (
   <Channel {...args} />
 );
