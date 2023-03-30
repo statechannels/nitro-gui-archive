64da9a9241e18b945740d8bc93d9290bedac6a92
George Knee <georgeknee@googlemail.com>
2023-03-30 22:09:00 +0100

    yarn prettier -w .

--- packages/site/src/components/NetworkBalance.stories.tsx
+++ packages/site/src/components/NetworkBalance.stories.tsx
@@ -11,52 +11,47 @@ const Template: ComponentStory<typeof NetworkBalance> = (
   args: NetworkBalanceProps,
 ) => <NetworkBalance {...args} />;
 
-export const EvenStart = Template.bind(this,{
-    myBalanceFree: 100n,
-    myBalanceLocked: 0n,
-    hubBalanceFree: 100n,
-    hubBalanceLocked: 0n,
-  });
+export const EvenStart = Template.bind(this, {
+  myBalanceFree: 100n,
+  myBalanceLocked: 0n,
+  hubBalanceFree: 100n,
+  hubBalanceLocked: 0n,
+});
 
-export const Mixed = Template.bind(this,{
-    myBalanceFree: 47n,
-    myBalanceLocked: 5n,
-    hubBalanceFree: 100n,
-    hubBalanceLocked: 11n,
-  },
-);
+export const Mixed = Template.bind(this, {
+  myBalanceFree: 47n,
+  myBalanceLocked: 5n,
+  hubBalanceFree: 100n,
+  hubBalanceLocked: 11n,
+});
 
-export const ClientStart = Template.bind(this,{
-    myBalanceFree: 100n,
-    myBalanceLocked: 0n,
-    hubBalanceFree: 0n,
-    hubBalanceLocked: 0n,
-  },
-);
+export const ClientStart = Template.bind(this, {
+  myBalanceFree: 100n,
+  myBalanceLocked: 0n,
+  hubBalanceFree: 0n,
+  hubBalanceLocked: 0n,
+});
 
-export const ClientMid = Template.bind(this,{
-    myBalanceFree: 70n,
-    myBalanceLocked: 5n,
-    hubBalanceFree: 10n,
-    hubBalanceLocked: 15n,
-  },
-);
+export const ClientMid = Template.bind(this, {
+  myBalanceFree: 70n,
+  myBalanceLocked: 5n,
+  hubBalanceFree: 10n,
+  hubBalanceLocked: 15n,
+});
 
-export const ProviderStart = Template.bind(this,{
-    myBalanceFree: 0n,
-    myBalanceLocked: 0n,
-    hubBalanceFree: 100n,
-    hubBalanceLocked: 0n,
-  },
-);
+export const ProviderStart = Template.bind(this, {
+  myBalanceFree: 0n,
+  myBalanceLocked: 0n,
+  hubBalanceFree: 100n,
+  hubBalanceLocked: 0n,
+});
 
-export const ProviderMid = Template.bind(this,{
-    myBalanceFree: 15n,
-    myBalanceLocked: 4n,
-    hubBalanceFree: 60n,
-    hubBalanceLocked: 21n,
-  },
-);
+export const ProviderMid = Template.bind(this, {
+  myBalanceFree: 15n,
+  myBalanceLocked: 4n,
+  hubBalanceFree: 60n,
+  hubBalanceLocked: 21n,
+});
 
 export const Zeros = Template.bind({
   myBalanceFree: 0n,
