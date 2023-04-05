e7acbc876c7c59b89048b7b421c3810278ac1ddf
Colin Kennedy <colin.kennedy@mesh.xyz>
2023-04-05 12:19:02 -0300

    enable controls on storybook renders
    ... now storybook lets you change some of the props during render time.

--- packages/site/src/components/NetworkBalance.stories.tsx
+++ packages/site/src/components/NetworkBalance.stories.tsx
@@ -1,101 +1,134 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { NetworkBalance, NetworkBalanceProps, VirtualChannelBalanceProps } from './NetworkBalance';
+import { Meta, StoryObj } from '@storybook/react';
+import {
+  NetworkBalance,
+  NetworkBalanceProps,
+  VirtualChannelBalanceProps,
+} from './NetworkBalance';
 
-export default {
+const meta: Meta<typeof NetworkBalance> = {
   title: 'NetworkBalance',
   component: NetworkBalance,
-} as ComponentMeta<typeof NetworkBalance>;
+};
+export default meta;
 
-const Template: ComponentStory<typeof NetworkBalance> = (
-  args: NetworkBalanceProps,
-) => <NetworkBalance {...args} />;
+type NB = StoryObj<NetworkBalanceProps>;
 
-export const Zeros = Template.bind(this, {
-  myBalanceFree: 0n,
-  theirBalanceFree: 0n,
-  status: 'running',
-  lockedBalances: [],
-});
+export const Controls: NB = {
+  args: {
+    myBalanceFree: 100n,
+    theirBalanceFree: 100n,
+    status: 'running',
+    lockedBalances: [],
+  },
+};
 
-export const EvenStart = Template.bind(this, {
-  myBalanceFree: 10n ** 18n,
-  theirBalanceFree: 10n ** 18n,
-  status: 'running',
-  lockedBalances: [],
-});
+export const Zeros: NB = {
+  args: {
+    myBalanceFree: 0n,
+    theirBalanceFree: 0n,
+    status: 'running',
+    lockedBalances: [],
+  },
+};
 
-export const ClientStart = Template.bind(this, {
-  myBalanceFree: 100n,
-  theirBalanceFree: 0n,
-  status: 'running',
-  lockedBalances: [],
-});
+export const EvenStart: NB = {
+  args: {
+    myBalanceFree: 10n ** 18n,
+    theirBalanceFree: 10n ** 18n,
+    status: 'running',
+    lockedBalances: [],
+  },
+};
 
-export const ClientMid = Template.bind(this, {
-  myBalanceFree: 70n,
-  theirBalanceFree: 10n,
-  status: 'running',
-  lockedBalances: [],
-});
+export const ClientStart: NB = {
+  args: {
+    myBalanceFree: 100n,
+    theirBalanceFree: 0n,
+    status: 'running',
+    lockedBalances: [],
+  },
+};
 
-export const ProviderStart = Template.bind(this, {
-  myBalanceFree: 0n,
-  theirBalanceFree: 100n,
-  status: 'running',
-  lockedBalances: [],
-});
+export const ClientMid: NB = {
+  args: {
+    myBalanceFree: 70n,
+    theirBalanceFree: 10n,
+    status: 'running',
+    lockedBalances: [],
+  },
+};
 
-export const ProviderMid = Template.bind(this, {
-  myBalanceFree: 15n,
-  theirBalanceFree: 60n,
-  status: 'running',
-  lockedBalances: [],
-});
+export const ProviderStart: NB = {
+  args: {
+    myBalanceFree: 0n,
+    theirBalanceFree: 100n,
+    status: 'running',
+    lockedBalances: [],
+  },
+};
 
-export const TwoChannels = Template.bind(this, {
-  myBalanceFree: 47n,
-  theirBalanceFree: 100n,
-  status: 'running',
-  lockedBalances: [
-    {
-      budget: 10n,
-      myPercentage: 0.5,
-    },
-    {
-      budget: 30n,
-      myPercentage: 0.2,
-    },
-  ],
-});
+export const ProviderMid: NB = {
+  args: {
+    myBalanceFree: 15n,
+    theirBalanceFree: 60n,
+    status: 'running',
+    lockedBalances: [],
+  },
+};
 
-export const SomeChannels = Template.bind(this, {
-  myBalanceFree: 50n,
-  theirBalanceFree: 100n,
-  status: 'running',
-  lockedBalances: randomChannels(5, 100n),
-});
+export const TwoChannels: NB = {
+  args: {
+    myBalanceFree: 47n,
+    theirBalanceFree: 100n,
+    status: 'running',
+    lockedBalances: [
+      {
+        budget: 10n,
+        myPercentage: 0.5,
+      },
+      {
+        budget: 30n,
+        myPercentage: 0.2,
+      },
+    ],
+  },
+};
 
-export const ManyChannels = Template.bind(this, {
-  myBalanceFree: 345n,
-  theirBalanceFree: 123n,
-  status: 'running',
-  lockedBalances: randomChannels(15, 150n),
-});
+export const SomeChannels: NB = {
+  args: {
+    myBalanceFree: 50n,
+    theirBalanceFree: 100n,
+    status: 'running',
+    lockedBalances: randomChannels(5, 100n),
+  },
+};
 
-export const UnresponsivePeer = Template.bind(this, {
-  myBalanceFree: 25n,
-  theirBalanceFree: 65n,
-  status: 'unresponsive-peer',
-  lockedBalances: randomChannels(5, 100n),
-});
+export const ManyChannels: NB = {
+  args: {
+    myBalanceFree: 345n,
+    theirBalanceFree: 123n,
+    status: 'running',
+    lockedBalances: randomChannels(15, 150n),
+  },
+};
 
-export const UnderChallenge = Template.bind(this, {
-  myBalanceFree: 83n,
-  theirBalanceFree: 24n,
-  status: 'under-challenge',
-  lockedBalances: randomChannels(5, 100n),
-});
+export const UnresponsivePeer: NB = {
+  args: {
+    myBalanceFree: 25n,
+    theirBalanceFree: 65n,
+    status: 'unresponsive-peer',
+    lockedBalances: randomChannels(5, 100n),
+  },
+};
+
+export const UnderChallenge: NB = {
+  args: {
+    myBalanceFree: 83n,
+    theirBalanceFree: 24n,
+    status: 'under-challenge',
+    lockedBalances: randomChannels(5, 100n),
+  },
+};
 
 function randomChannels(
   numChannels: number,
